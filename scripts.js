caa46dd250ee81d17b79234213f86ac4761f7b66
maluya55 <126713130+maluya55@users.noreply.github.com>
2023-03-01 16:43:51 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,11 @@
+const datos = document.getElementById('datos1');
+datos.addEventListener('mouseover',function handleMouseOver(){datos.style.backgroundColor='lightgrey'});
+datos.addEventListener('mouseout',function handleMouseOut(){datos.style.backgroundColor='white'});
+
+const forma = document.getElementById("forma");
+forma.addEventListener('mouseover',function handleMouseOver(){forma.style.backgroundColor='lightgrey'});
+forma.addEventListener('mouseout',function handleMouseOut(){forma.style.backgroundColor='white'});
+
+const exper = document.getElementById("exper");
+exper.addEventListener('mouseover',function handleMouseOver(){exper.style.backgroundColor='lightgrey'});
+exper.addEventListener('mouseout',function handleMouseOut(){exper.style.backgroundColor='white'});
